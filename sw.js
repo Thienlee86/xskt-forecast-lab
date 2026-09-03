577fa61659809ecbbb0ab93962f40951711d7a7d
Thienlee86 <ltpthien86@gmail.com>
2026-09-03 13:07:35 +0700

    fix: cache versioned challenger modules

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="xskt-lab-v16-champion-challenger";const ASSETS=["./","./index.html","./styles.css?v=9","./src/app.js?v=15","./src/data.js","./data/xsmn_seed.json?v=20260902","./src/model.js","./src/model6.js","./src/model-prizes.js","./src/backtest.js","./src/backtest6.js","./src/backtest-prizes.js","./src/storage.js"];
+const CACHE="xskt-lab-v17-module-versions";const ASSETS=["./","./index.html","./styles.css?v=9","./src/app.js?v=16","./src/data.js","./data/xsmn_seed.json?v=20260902","./src/model.js","./src/model6.js","./src/model-prizes.js?v=2","./src/backtest.js","./src/backtest6.js","./src/backtest-prizes.js?v=2","./src/storage.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
